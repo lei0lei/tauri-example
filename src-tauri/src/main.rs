0c596a65792863a3a4cad092abb028bd9af46df0
meng fanlei <lei.lei.fan.meng@gmail.com>
2024-11-18 11:00:24 +0800

    .

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
 fn main() {
-    tauri_example_lib::run()
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_app::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_shell::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
